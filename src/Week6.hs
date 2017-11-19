a77a2edb7c7e681e2b5d13bc10d307872a45c119
zach007 <zach007cn@gmail.com>
2017-11-19 12:37:51 +0000

    ADT

---
+++ src/Week6.hs
@@ -0,0 +1,36 @@
+module Week6 where
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib(n-2)
+
+
+fibs1 :: [Integer]
+fibs1 = map fib [0,1..]
+{-
+  take 5 element of fibsl
+
+  fibsl = fib 4 + fib 3
+        = fib 3 + fib 2  + fib 2 + fib 1
+        = fib 2 + fib 1 + fib 1 + fib 0 + fib 1 + fib 0 + fib1
+-}
+
+data Stream a = Cons a (Stream a)
+
+instance Show a => Show (Stream a )where
+    show = show . take 20 .streamToList
+
+streamToList :: Stream a -> [a]
+streamToList (Cons y c) = y : streamToList c
+
+
+streamRepeat :: a -> Stream a
+streamRepeat n = Cons n (streamRepeat n)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons y ys) = Cons (f y) (streamMap  f ys)
+
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f y = Cons y (streamFromSeed f (f y))
